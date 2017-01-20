a27a8f8d78a41db4402ed6b22c23134e18267761
Outernemesis <outernemesis.00@web.de>
2017-01-21 00:02:57 +0100

    Test food und test plate hinzugefügt

--- Be The Wave/Assets/_Scripts/Game/Food.cs
+++ Be The Wave/Assets/_Scripts/Game/Food.cs
@@ -6,23 +6,35 @@ public class Food : MonoBehaviour
 {
 
     #region Fields
+    
 
+    public int m_reqStrengthLv;
+    
+    public int m_actDegreePec;
+    public int m_degreePecRaise;
 
+    public int m_actBurnPec;
+    public int m_burnPecRaise;
+
+    int m_neededDegree = 100;
 
     #endregion
 
     #region Methods
 
+
+
     // Use this for initialization
     void Start()
     {
-
+        m_actDegreePec = 0;
+        m_actBurnPec = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.forward = -Camera.main.transform.forward;
     }
 
     #endregion
--- Be The Wave/Assets/_Scripts/Game/Plate.cs
+++ Be The Wave/Assets/_Scripts/Game/Plate.cs
@@ -7,6 +7,10 @@ public class Plate : MonoBehaviour
 
     #region Fields
 
+    public float rotSpeed = 10;
+
+    public float distance = 10;
+
     [SerializeField]
     private Food[] m_foods;
 
@@ -18,13 +22,23 @@ public class Plate : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        if (m_foods.Length <= 0)
+            return;
 
+        float angle = 360 / m_foods.Length;
+
+        for (int i = 0; i < m_foods.Length; i++)
+        {
+            Vector3 dir = new Vector3(-Mathf.Sin(2 * Mathf.PI * i / m_foods.Length), 0, Mathf.Cos(2 * Mathf.PI * i / m_foods.Length));
+            dir = dir * distance;
+            Instantiate(m_foods[i].gameObject, this.transform.position + dir, Quaternion.identity, this.transform);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        this.transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
     }
 
     #endregion
