f631e0a12318b3e15bcea0c6510f18bfb1b7bb39
Fell <fell@fellr.net>
2017-01-20 23:19:54 +0100

    timer should be done

--- Be The Wave/Assets/Timer.cs
+++ Be The Wave/Assets/Timer.cs
@@ -1,29 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Timer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private float m_time;
+    private bool m_ticking = true;
+
+    public float Time
+    {
+        get
+        {
+            return m_time;
+        }
+
+        set
+        {
+            m_time = value;
+        }
+    }
+
+    public bool Ticking
+    {
+        get
+        {
+            return m_ticking;
+        }
+
+        set
+        {
+            m_ticking = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        Time = 300; // 5:00
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        TextMesh theMesh = gameObject.GetComponent<TextMesh>();
+	void Update () { 
+        if (m_ticking && (int)Time > 0)
+        {
+            // Update Time
+            Time -= UnityEngine.Time.deltaTime;
 
-        string newText = "00";
+            // Update Text
+            TextMesh theMesh = gameObject.GetComponent<TextMesh>();
+            theMesh.text = getTimeString();
+        }
+    }
 
-        int seconds = (int)Time.realtimeSinceStartup;
+    private string getTimeString() {
+        int minutes = (int)(Time / 60);
+        int seconds = (int)(Time % 60);
 
-        if(seconds % 2 == 0)
-            newText += ":";
+        if (seconds % 2 == 0)
+            return minutes.ToString("D2") + ":" + seconds.ToString("D2");
         else
-            newText += " ";
-
-        newText += seconds.ToString("D2"); ;
-
-        theMesh.text = newText;
-	}
+            return minutes.ToString("D2") + " " + seconds.ToString("D2");
+    }
 }
