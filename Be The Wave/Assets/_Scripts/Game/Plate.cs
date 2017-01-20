3e14b66b833bca8fef7092fd966ec9b7fd347dae
equusligneus <reher.frederik@gmail.com>
2017-01-21 00:12:54 +0100

    fixed errors

--- Be The Wave/Assets/_Scripts/Game/Plate.cs
+++ Be The Wave/Assets/_Scripts/Game/Plate.cs
@@ -21,23 +21,23 @@ public class Plate : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        if (m_foods.Length <= 0)
+        if ( m_foods.Count <= 0 )
             return;
 
-        float angle = 360 / m_foods.Length;
+        float angle = 360 / m_foods.Count;
 
-        for (int i = 0; i < m_foods.Length; i++)
+        for ( int i = 0; i < m_foods.Count; i++ )
         {
-            Vector3 dir = new Vector3(-Mathf.Sin(2 * Mathf.PI * i / m_foods.Length), 0, Mathf.Cos(2 * Mathf.PI * i / m_foods.Length));
+            Vector3 dir = new Vector3( -Mathf.Sin( 2 * Mathf.PI * i / m_foods.Count ), 0, Mathf.Cos( 2 * Mathf.PI * i / m_foods.Count ) );
             dir = dir * distance;
-            Instantiate(m_foods[i].gameObject, this.transform.position + dir, Quaternion.identity, this.transform);
+            Instantiate( m_foods[ i ].gameObject, this.transform.position + dir, Quaternion.identity, this.transform );
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
+        this.transform.Rotate( Vector3.up, rotSpeed * Time.deltaTime );
     }
 
     #endregion
