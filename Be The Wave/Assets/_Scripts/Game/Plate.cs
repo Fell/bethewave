0882c0f934874fc5ed5a75d86c50242048065508
equusligneus <reher.frederik@gmail.com>
2017-01-20 21:10:06 +0100

    added some basic code files

---
+++ Be The Wave/Assets/_Scripts/Game/Plate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plate : MonoBehaviour
+{
+
+    #region Fields
+
+    [SerializeField]
+    private Food[] m_foods;
+
+    #endregion
+
+
+    #region Methods
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #endregion
+}
